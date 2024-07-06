5c628f043e0f5a4d3b5f09c08c0a90a60d68e67d
Harish <bhattaharish10@gmail.com>
2024-07-07 00:07:58 +0545

    Imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-
-const App: React.FC = () => {
-  return <div></div>;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Join from "./components/Join";
+import Chat from "./components/chat";
+const App = () => {
+  <Router>
+    <Routes>
+      <Route path="/" element={<Join />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
+  </Router>;
 };
 
 export default App;
