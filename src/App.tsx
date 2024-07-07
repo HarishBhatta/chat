475e0ba7ea63a4a6d30a95922258d036e4de6732
Harish <bhattaharish10@gmail.com>
2024-07-07 23:55:00 +0545

    Main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Join from "./components/Join";
 import Chat from "./components/chat";
-const App = () => {
+const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Join />} />
       <Route path="/chat" element={<Chat />} />
     </Routes>
-  </Router>;
-};
+  </Router>
+);
 
 export default App;
